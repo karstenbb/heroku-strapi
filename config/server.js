b87d2e26292cadf722c8403a37302410242c9ad3
Karsten Bjelde <fotball-karsten@hotmail.com>
2021-04-14 14:26:43 +0200

    init project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '336de48b372881fe427ef9627a4b6c86'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '512d8e73-c9e7-4ffd-a0b1-36a2e8cce7c9'
+};
